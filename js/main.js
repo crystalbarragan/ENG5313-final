c26ed640ebf2d7e8a0741e884a9d986906a467f0
Crystal Barragán <crystalbarragan@icloud.com>
2016-12-06 23:25:22 -0600

    Added typed.js; other text edits

--- js/main.js
+++ js/main.js
@@ -65,4 +65,42 @@ setInterval(function () {
 
     wow.init();
 
+// Typed.js
+    $(".site-desc").typed({
+    strings: ["We write your user instructions.", "We create your product manuals."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 50,
+    // time before typing starts
+    startDelay: 50,
+    // backspacing speed
+    backSpeed: 0,
+    // shuffle the strings
+    shuffle: false,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: false,
+    // false = infinite
+    loopCount: false,
+    // show cursor
+    showCursor: true,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+    });
+
+
 });
